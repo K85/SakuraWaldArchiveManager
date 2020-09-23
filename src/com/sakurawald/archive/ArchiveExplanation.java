57b50087114cbb3561f05f6cca97c391b28a77e4
HakuzosuSAMA <3172906506@qq.com>
2020-09-23 10:09:55 +0800

    发布版本

--- src/com/sakurawald/archive/ArchiveExplanation.java
+++ src/com/sakurawald/archive/ArchiveExplanation.java
@@ -52,105 +52,107 @@ public class ArchiveExplanation {
          * 用户数据
          */
         result.add(new ArchiveExplanation("用户列表", "USERS", "users.dat"));
-        result.add(new ArchiveExplanation("单个用户数据", "SINGLE_USER", "$REGEXuser\\d+?\\.dat"));
+        result.add(new ArchiveExplanation("单个用户数据", "SINGLE_USER", USE_REGEX_SIGN + "user\\d+?\\.dat"));
 
         /**
          * 冒险模式
+         * "game1
+         * USE_REGEX_SIGN + "game\\d+?
          */
-        result.add(new ArchiveExplanation("冒险模式", "ADVENTURE", "game1_0.dat"));
+        result.add(new ArchiveExplanation("冒险模式", "ADVENTURE", USE_REGEX_SIGN + "game\\d+?_0\\.dat"));
 
         /**
          * 生存模式
          */
         //有尽模式
-        result.add(new ArchiveExplanation("生存模式_白天", "SURVIVAL_DAY", "game1_1.dat"));
-        result.add(new ArchiveExplanation("生存模式_黑夜", "SURVIVAL_NIGHT", "game1_2.dat"));
-        result.add(new ArchiveExplanation("生存模式_浓雾", "SURVIVAL_FOG", "game1_4.dat"));
-        result.add(new ArchiveExplanation("生存模式_屋顶", "SURVIVAL_ROOF", "game1_5.dat"));
-        result.add(new ArchiveExplanation("生存模式_白天_困难", "SURVIVAL_DAY_HARD", "game1_6.dat"));
-        result.add(new ArchiveExplanation("生存模式_黑夜_困难", "SURVIVAL_NIGHT_HARD", "game1_7.dat"));
-        result.add(new ArchiveExplanation("生存模式_泳池_困难", "SURVIVAL_POOL_HARD", "game1_8.dat"));
-        result.add(new ArchiveExplanation("生存模式_浓雾_困难", "SURVIVAL_FOG_HARD", "game1_9.dat"));
-        result.add(new ArchiveExplanation("生存模式_屋顶_困难", "SURVIVAL_ROOF_HARD", "game1_10.dat"));
+        result.add(new ArchiveExplanation("生存模式_白天", "SURVIVAL_DAY", USE_REGEX_SIGN + "game\\d+?_1\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_黑夜", "SURVIVAL_NIGHT", USE_REGEX_SIGN + "game\\d+?_2\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_浓雾", "SURVIVAL_FOG", USE_REGEX_SIGN + "game\\d+?_4\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_屋顶", "SURVIVAL_ROOF", USE_REGEX_SIGN + "game\\d+?_5\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_白天_困难", "SURVIVAL_DAY_HARD", USE_REGEX_SIGN + "game\\d+?_6\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_黑夜_困难", "SURVIVAL_NIGHT_HARD", USE_REGEX_SIGN + "game\\d+?_7\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_泳池_困难", "SURVIVAL_POOL_HARD", USE_REGEX_SIGN + "game\\d+?_8\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_浓雾_困难", "SURVIVAL_FOG_HARD", USE_REGEX_SIGN + "game\\d+?_9\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_屋顶_困难", "SURVIVAL_ROOF_HARD", USE_REGEX_SIGN + "game\\d+?_10\\.dat"));
         // 无尽模式
-        result.add(new ArchiveExplanation("生存模式_白天：(无尽版)", "SURVIVAL_DAY_ENDLESS", "game1_11.dat"));
-        result.add(new ArchiveExplanation("生存模式_黑夜：(无尽版)", "SURVIVAL_NIGHT_ENDLESS", "game1_12.dat"));
-        result.add(new ArchiveExplanation("生存模式_浓雾：(无尽版)", "SURVIVAL_FOG_ENDLESS", "game1_14.dat"));
-        result.add(new ArchiveExplanation("生存模式_泳池：(无尽版)", "SURVIVAL_POOL_ENDLESS", "game1_13.dat"));
-        result.add(new ArchiveExplanation("生存模式_屋顶：(无尽版)", "SURVIVAL_ROOF_ENDLESS", "game1_15.dat"));
+        result.add(new ArchiveExplanation("生存模式_白天：(无尽版)", "SURVIVAL_DAY_ENDLESS", USE_REGEX_SIGN + "game\\d+?_11\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_黑夜：(无尽版)", "SURVIVAL_NIGHT_ENDLESS", USE_REGEX_SIGN + "game\\d+?_12\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_浓雾：(无尽版)", "SURVIVAL_FOG_ENDLESS", USE_REGEX_SIGN + "game\\d+?_14\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_泳池：(无尽版)", "SURVIVAL_POOL_ENDLESS", USE_REGEX_SIGN + "game\\d+?_13\\.dat"));
+        result.add(new ArchiveExplanation("生存模式_屋顶：(无尽版)", "SURVIVAL_ROOF_ENDLESS", USE_REGEX_SIGN + "game\\d+?_15\\.dat"));
 
         /**
          * 迷你游戏模式
          */
 
-        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球", "MINI_GAMES_WALL_NUT_BOWLING", "game1_17.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_僵尸快跑", "MINI_GAMES_ZOMBIE_NIMBLE_ZOMBIE_QUICK", "game1_29.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_小僵尸大麻烦", "MINI_GAMES_BIG_TROUBLE_LITTLE_ZOMBIE", "game1_24.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_植物僵尸", "MINI_GAMES_ZOM_BOTANY", "game1_16.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_老虎机", "MINI_GAMES_SLOT_MACHINE", "game1_18.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_雨中种植物", "MINI_GAMES_ITS_RAINING_SEEDS", "game1_19.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_宝石迷阵", "MINI_GAMES_BEGHOULED", "game1_20.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_隐形食脑者", "MINI_GAMES_INVISI_GHOUL", "game1_21.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_看星星", "MINI_GAMES_SEEING_STARS", "game1_22.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_僵尸水族馆", "MINI_GAMES_ZOMBIQUARIUM", "game1_23.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_宝石迷阵2", "MINI_GAMES_BEGHOULED_TWIST", "game1_24.dat"));
-//        result.add(new ArchiveExplanation("迷你游戏_传送门", "MINI_GAMES_PORTAL_COMBAT","game1_25.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_保护传送门", "MINI_GAMES_PORTAL_COMBAT", "game1_26.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_你看，他们像柱子一样", "MINI_GAMES_COLUMN_LIKE_YOU_SEE_EM", "game1_27.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_雪橇区", "MINI_GAMES_BOBSLED_BONANZA", "game1_28.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_锤僵尸", "MINI_GAMES_WHACK_A_ZOMBIE", "game1_30.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_植物僵尸2", "MINI_GAMES_ZOM_BOTANY_2", "game1_32.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_谁笑到最后", "MINI_GAMES_LAST_STAND", "game1_31.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球2", "MINI_GAMES_WALL_NUT_BOWLING_2", "game1_33.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_跳跳舞会", "MINI_GAMES_POGO_PARTY", "game1_34.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_僵王博士的复仇", "MINI_GAMES_DR_ZOMBOSS_REVENGE", "game1_35.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球", "MINI_GAMES_WALL_NUT_BOWLING", USE_REGEX_SIGN + "game\\d+?_17\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_僵尸快跑", "MINI_GAMES_ZOMBIE_NIMBLE_ZOMBIE_QUICK", USE_REGEX_SIGN + "game\\d+?_29\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_小僵尸大麻烦", "MINI_GAMES_BIG_TROUBLE_LITTLE_ZOMBIE", USE_REGEX_SIGN + "game\\d+?_24\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_植物僵尸", "MINI_GAMES_ZOM_BOTANY", USE_REGEX_SIGN + "game\\d+?_16\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_老虎机", "MINI_GAMES_SLOT_MACHINE", USE_REGEX_SIGN + "game\\d+?_18\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_雨中种植物", "MINI_GAMES_ITS_RAINING_SEEDS", USE_REGEX_SIGN + "game\\d+?_19\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_宝石迷阵", "MINI_GAMES_BEGHOULED", USE_REGEX_SIGN + "game\\d+?_20\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_隐形食脑者", "MINI_GAMES_INVISI_GHOUL", USE_REGEX_SIGN + "game\\d+?_21\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_看星星", "MINI_GAMES_SEEING_STARS", USE_REGEX_SIGN + "game\\d+?_22\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_僵尸水族馆", "MINI_GAMES_ZOMBIQUARIUM", USE_REGEX_SIGN + "game\\d+?_23\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_宝石迷阵2", "MINI_GAMES_BEGHOULED_TWIST", USE_REGEX_SIGN + "game\\d+?_24\\.dat"));
+//        result.add(new ArchiveExplanation("迷你游戏_传送门", "MINI_GAMES_PORTAL_COMBAT",USE_REGEX_SIGN + "game\\d+?_25\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_保护传送门", "MINI_GAMES_PORTAL_COMBAT", USE_REGEX_SIGN + "game\\d+?_26\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_你看，他们像柱子一样", "MINI_GAMES_COLUMN_LIKE_YOU_SEE_EM", USE_REGEX_SIGN + "game\\d+?_27\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_雪橇区", "MINI_GAMES_BOBSLED_BONANZA", USE_REGEX_SIGN + "game\\d+?_28\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_锤僵尸", "MINI_GAMES_WHACK_A_ZOMBIE", USE_REGEX_SIGN + "game\\d+?_30\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_植物僵尸2", "MINI_GAMES_ZOM_BOTANY_2", USE_REGEX_SIGN + "game\\d+?_32\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_谁笑到最后", "MINI_GAMES_LAST_STAND", USE_REGEX_SIGN + "game\\d+?_31\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球2", "MINI_GAMES_WALL_NUT_BOWLING_2", USE_REGEX_SIGN + "game\\d+?_33\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_跳跳舞会", "MINI_GAMES_POGO_PARTY", USE_REGEX_SIGN + "game\\d+?_34\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_僵王博士的复仇", "MINI_GAMES_DR_ZOMBOSS_REVENGE", USE_REGEX_SIGN + "game\\d+?_35\\.dat"));
 
         /**
          * 迷你游戏模式 >> 隐藏游戏
          */
-        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球艺术锦标赛", "ART_CHALLENGE_WALL_NUT", "game1_36.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_晴天", "MINI_GAMES_SUNNY_DAY", "game1_37.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_无草皮之地", "MINI_GAMES_UNSODDED", "game1_38.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_重要时间", "MINI_GAMES_BIG_TIME", "game1_39.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_挑战种太阳花的艺术", "MINI_GAMES_ART_CHALLENGE_SUNFLOWER", "game1_40.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_空袭", "MINI_GAMES_AIR_RAID", "game1_41.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_坚果保龄球艺术锦标赛", "ART_CHALLENGE_WALL_NUT", USE_REGEX_SIGN + "game\\d+?_36\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_晴天", "MINI_GAMES_SUNNY_DAY", USE_REGEX_SIGN + "game\\d+?_37\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_无草皮之地", "MINI_GAMES_UNSODDED", USE_REGEX_SIGN + "game\\d+?_38\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_重要时间", "MINI_GAMES_BIG_TIME", USE_REGEX_SIGN + "game\\d+?_39\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_挑战种太阳花的艺术", "MINI_GAMES_ART_CHALLENGE_SUNFLOWER", USE_REGEX_SIGN + "game\\d+?_40\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_空袭", "MINI_GAMES_AIR_RAID", USE_REGEX_SIGN + "game\\d+?_41\\.dat"));
         // 注意: 隐藏关_冰冻关卡 没有存档机制
-        result.add(new ArchiveExplanation("迷你游戏_超乎寻常的压力", "MINI_GAMES_HIGH_GRAVITY ", "game1_44.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_坟墓模式", "MINI_GAMES_GRAVE_DANGER", "game1_45.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_你能把它挖出来吗？", "MINI_GAMES_CAN_U_DIG_IT?", "game1_46.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_黑暗的暴风雨夜", "MINI_GAMES_DARK_STORMY_NIGHT", "game1_47.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_蹦极闪电战", "MINI_GAMES_BUNGEE_BLITZ", "game1_48.dat"));
-        result.add(new ArchiveExplanation("迷你游戏_Squirrel", "MINI_GAMES_SQUIRRE", "game1_49.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_超乎寻常的压力", "MINI_GAMES_HIGH_GRAVITY ", USE_REGEX_SIGN + "game\\d+?_44\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_坟墓模式", "MINI_GAMES_GRAVE_DANGER", USE_REGEX_SIGN + "game\\d+?_45\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_你能把它挖出来吗？", "MINI_GAMES_CAN_U_DIG_IT?", USE_REGEX_SIGN + "game\\d+?_46\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_黑暗的暴风雨夜", "MINI_GAMES_DARK_STORMY_NIGHT", USE_REGEX_SIGN + "game\\d+?_47\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_蹦极闪电战", "MINI_GAMES_BUNGEE_BLITZ", USE_REGEX_SIGN + "game\\d+?_48\\.dat"));
+        result.add(new ArchiveExplanation("迷你游戏_Squirrel", "MINI_GAMES_SQUIRRE", USE_REGEX_SIGN + "game\\d+?_49\\.dat"));
 
         /**
          * 解谜模式
          */
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_破罐者", "PUZZLE_VASEBREAKER_VASEBREAKER", "game1_51.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_全部留下", "PUZZLE_VASEBREAKER_TO_THE_LEFT", "game1_52.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_第三个罐子", "PUZZLE_VASEBREAKER_THIRD_VASE", "game1_53.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_连锁反应", "PUZZLE_VASEBREAKER_CHAIN_REACTION", "game1_54.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_M的意思是金属", "PUZZLE_VASEBREAKER_M_IS_FOR_METAL", "game1_55.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_胆怯的制陶工", "PUZZLE_VASEBREAKER_SCARY_POTTER", "game1_56.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_变戏法", "PUZZLE_VASEBREAKER_HOKEY_POKEY", "game1_57.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_另一个连锁反应", "PUZZLE_VASEBREAKER_ANOTHER_CHAIN_REACTION", "game1_58.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_王牌罐子", "PUZZLE_VASEBREAKER_ACE_OF_VASE", "game1_59.dat"));
-        result.add(new ArchiveExplanation("解谜模式_砸罐子_无尽", "PUZZLE_VASEBREAKER_VASEBREAKER_ENDLESS", "game1_60.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我是僵尸", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE", "game1_61.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我也是僵尸", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE_TOO", "game1_62.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_你能铲了它吗", "PUZZLE_I_AM_A_ZOMBIE_CAN_YOU_DIG_IT", "game1_63.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_完全傻了", "PUZZLE_I_AM_A_ZOMBIE_TOTALLY_NUTS", "game1_64.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_死亡飞艇", "PUZZLE_I_AM_A_ZOMBIE_DEAD_ZEPPELIN", "game1_65.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我烂了", "PUZZLE_I_AM_A_ZOMBIE_ME_SMASH", "game1_66.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_僵尸摇摆", "PUZZLE_I_AM_A_ZOMBIE_ZOM_BOOGIE", "game1_67.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_三连击", "PUZZLE_I_AM_A_ZOMBIE_THREE_HIT_WONDER", "game1_68.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_你所有的大脑都是属于我的", "PUZZLE_I_AM_A_ZOMBIE_ALL_YOUR_BRAINZ_R_BELONG_TO_US", "game1_69.dat"));
-        result.add(new ArchiveExplanation("解谜模式_我是僵尸_无尽", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE_ENDLESS", "game1_70.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_破罐者", "PUZZLE_VASEBREAKER_VASEBREAKER", USE_REGEX_SIGN + "game\\d+?_51\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_全部留下", "PUZZLE_VASEBREAKER_TO_THE_LEFT", USE_REGEX_SIGN + "game\\d+?_52\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_第三个罐子", "PUZZLE_VASEBREAKER_THIRD_VASE", USE_REGEX_SIGN + "game\\d+?_53\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_连锁反应", "PUZZLE_VASEBREAKER_CHAIN_REACTION", USE_REGEX_SIGN + "game\\d+?_54\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_M的意思是金属", "PUZZLE_VASEBREAKER_M_IS_FOR_METAL", USE_REGEX_SIGN + "game\\d+?_55\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_胆怯的制陶工", "PUZZLE_VASEBREAKER_SCARY_POTTER", USE_REGEX_SIGN + "game\\d+?_56\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_变戏法", "PUZZLE_VASEBREAKER_HOKEY_POKEY", USE_REGEX_SIGN + "game\\d+?_57\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_另一个连锁反应", "PUZZLE_VASEBREAKER_ANOTHER_CHAIN_REACTION", USE_REGEX_SIGN + "game\\d+?_58\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_王牌罐子", "PUZZLE_VASEBREAKER_ACE_OF_VASE", USE_REGEX_SIGN + "game\\d+?_59\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_砸罐子_无尽", "PUZZLE_VASEBREAKER_VASEBREAKER_ENDLESS", USE_REGEX_SIGN + "game\\d+?_60\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我是僵尸", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE", USE_REGEX_SIGN + "game\\d+?_61\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我也是僵尸", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE_TOO", USE_REGEX_SIGN + "game\\d+?_62\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_你能铲了它吗", "PUZZLE_I_AM_A_ZOMBIE_CAN_YOU_DIG_IT", USE_REGEX_SIGN + "game\\d+?_63\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_完全傻了", "PUZZLE_I_AM_A_ZOMBIE_TOTALLY_NUTS", USE_REGEX_SIGN + "game\\d+?_64\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_死亡飞艇", "PUZZLE_I_AM_A_ZOMBIE_DEAD_ZEPPELIN", USE_REGEX_SIGN + "game\\d+?_65\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_我烂了", "PUZZLE_I_AM_A_ZOMBIE_ME_SMASH", USE_REGEX_SIGN + "game\\d+?_66\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_僵尸摇摆", "PUZZLE_I_AM_A_ZOMBIE_ZOM_BOOGIE", USE_REGEX_SIGN + "game\\d+?_67\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_三连击", "PUZZLE_I_AM_A_ZOMBIE_THREE_HIT_WONDER", USE_REGEX_SIGN + "game\\d+?_68\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_你所有的大脑都是属于我的", "PUZZLE_I_AM_A_ZOMBIE_ALL_YOUR_BRAINZ_R_BELONG_TO_US", USE_REGEX_SIGN + "game\\d+?_69\\.dat"));
+        result.add(new ArchiveExplanation("解谜模式_我是僵尸_无尽", "PUZZLE_I_AM_A_ZOMBIE_ZOMBIE_ENDLESS", USE_REGEX_SIGN + "game\\d+?_70\\.dat"));
 
 
         /**
          * 自定义备份文件
          */
-        result.add(new ArchiveExplanation("你的自定义备份文件的注解", "YOUR_CUSTOM_FILE_SIGN", "your_custom_file_name.dat"));
+        result.add(new ArchiveExplanation("你的自定义备份文件的注解", "YOUR_CUSTOM_FILE_SIGN", "your_custom_file_name\\.dat"));
 
         return result;
     }
--- src/com/sakurawald/debug/Test.java
+++ src/com/sakurawald/debug/Test.java
@@ -5,19 +5,214 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
 
-//        int count = 0;
-//        CheatEngine ce = new CheatEngine("MainWindow", "Plants vs. Zombies", 7836920,4,
-//                new ArrayList<Long>(){
-//                    {
-//                        add(2152L);
-//                        add(184L);
-////                        add(36L);
-//                    }
-//                });
 
-//        count = ce.getValue().getValue();
-//        System.out.println("僵尸数量为: " + count);
+        //new File("D:\\.noupdate").createNewFile();
+//        System.out.println(generatePath("D:\\Game\\PlantsVsZombies\\存档\\阵心雕龙扑克存档\\主卡：地球文明\\2：阴阳通玄\\C2\\1", 4));
+//        helper3();
+
+//        renameToUser1();
+    }
+/*
+    public static void renameToUser1() throws  Exception {
+
+        searchAndRenameFiles("C:\\Users\\31729\\Desktop\\ArchiveManager懒人包\\ArchiveManager\\ArchiveBeans\\ConvenientArchives");
+
 
     }
+
+    public static void searchAndRenameFiles(String root_path) throws  Exception{
+
+        for (File file : new File(root_path).listFiles()) {
+
+            // 是文件夹则继续遍历
+            if (file.isDirectory() == true) {
+                searchFiles(file.getAbsolutePath());
+
+            } else{
+                // 是文件则判断
+                if (file.getName().contains(".dat") == true && file.getName().contains("_") == true) {
+
+//                    String output_path = null;
+//
+//                    output_path = generatePath(file.getParentFile().getAbsolutePath(), 4);
+//                    output_path = output_path + file.getName();
+//                    new File(output_path).getParentFile().mkdirs();
+
+                    // Copy
+                    String[] parts = file.getName().split("_");
+                    String newName = parts[0].replace("2", "1") + "_" + parts[1];
+
+                    file.renameTo(new File(file.getParentFile().getAbsolutePath() + "\\" + newName));
+
+                }
+            }
+
+
+
+        }
+
+    }
+
+    public static void rename () throws Exception{
+
+        searchFiles("D:\\Game\\PlantsVsZombies\\存档\\九章阵华录扑克存档");
+
+    }
+
+    public static void helper3() throws Exception{
+
+        searchFiles("D:\\Game\\PlantsVsZombies\\存档\\九章阵华录扑克存档");
+
+    }
+
+    public static void searchFiles(String root_path) throws  Exception{
+
+        for (File file : new File(root_path).listFiles()) {
+
+            // 是文件夹则继续遍历
+            if (file.isDirectory() == true) {
+                searchFiles(file.getAbsolutePath());
+
+            } else{
+                // 是文件则判断
+                if (file.getName().contains(".dat") == true) {
+
+                    String output_path = null;
+
+                    output_path = generatePath(file.getParentFile().getAbsolutePath(), 4);
+                    output_path = output_path + file.getName();
+                    new File(output_path).getParentFile().mkdirs();
+
+                    // Copy
+
+                    FileUtil.copyFile(file.getAbsolutePath(), output_path);
+
+                }
+            }
+
+
+
+        }
+
+    }
+
+    public static String generatePath(String end_path, int floors) {
+
+        String result = "D:\\out\\";
+
+
+
+        File f = new File(end_path);
+
+        String temp = f.getName();
+
+        // 遍历
+
+        int i = 0;
+
+        for (;i<floors; i++) {
+            temp =  f.getParentFile().getName() + "_" + temp;
+
+            f = f.getParentFile();
+        }
+
+        result = result + temp;
+
+        result = result + "\\ConvenientArchive\\";
+
+
+        String[] t = result.split("_", 2);
+        File f2 = new File(t[0]);
+        result = "D:\\out" + "\\" + "[" + f2.getName() + "] " + t[1];
+
+        return result;
+    }
+
+    public static void helper2() throws Exception{
+
+
+
+        File all_single_archive_file = new File("D:\\Game\\PlantsVsZombies\\存档\\年度加强版主卡存档\\地球文明扑克");
+
+        for (File singleSeriesFolder : all_single_archive_file.listFiles()) {
+
+//            System.out.println("名称为：" + singleFile.getAbsolutePath());
+
+            for (File one_file_in_one_series : singleSeriesFolder.listFiles()) {
+
+                // Analyse
+                String seriesName = one_file_in_one_series.getParentFile().getName();
+
+
+                // File Filter
+                if (one_file_in_one_series.getName().contains(".dat")) {
+                    File out = new File("D:\\out\\" + "[年度加强版主卡存档] " + seriesName + "\\ConvenientArchive\\" + one_file_in_one_series.getName());
+                  out.getParentFile().mkdirs();
+
+                  FileUtil.copyFile(one_file_in_one_series.getAbsolutePath(), out.getAbsolutePath());
+                }
+
+
+            }
+
+
+//            // DIM
+//            String archiveSeriesPrefix = "[" + "无尽存档合集V3.0b" +"] ";
+//
+//
+//            // Analyse
+//            String[] name_parts =singleFile.getName().split("\\.dat\\.");
+//            String seriesName = name_parts[1];
+//            String fileName = name_parts[0] + ".dat";
+//            System.out.println("seriesName = " + seriesName + ", fileName = " + fileName);
+//
+//
+//            // Output
+//            File out = new File("D:\\out\\" + archiveSeriesPrefix + seriesName + "\\ConvenientArchive\\" + singleFile.getName());
+//            out.getParentFile().mkdirs();
+//
+//            FileUtil.copyFile(singleFile.getAbsolutePath(), out.getAbsolutePath());
+//
+//            // Rename
+//            out.renameTo(new File(out.getParentFile().getAbsolutePath() + "\\" + fileName));
+
+        }
+
+    }
+
+
+
+    public static void helper1() throws Exception{
+
+
+
+        File all_single_archive_file = new File("D:\\Game\\PlantsVsZombies\\存档\\√ 无尽存档合集V3.0b\\V1.2.0.1073");
+
+        for (File singleFile : all_single_archive_file.listFiles()) {
+
+            // DIM
+            String archiveSeriesPrefix = "[" + "无尽存档合集V3.0b" +"] ";
+
+
+            // Analyse
+            String[] name_parts =singleFile.getName().split("\\.dat\\.");
+            String seriesName = name_parts[1];
+            String fileName = name_parts[0] + ".dat";
+            System.out.println("seriesName = " + seriesName + ", fileName = " + fileName);
+
+
+            // Output
+            File out = new File("D:\\out\\" + archiveSeriesPrefix + seriesName + "\\ConvenientArchive\\" + singleFile.getName());
+            out.getParentFile().mkdirs();
+
+            FileUtil.copyFile(singleFile.getAbsolutePath(), out.getAbsolutePath());
+
+            // Rename
+            out.renameTo(new File(out.getParentFile().getAbsolutePath() + "\\" + fileName));
+
+        }
+
+    }*/
+
 
 }
